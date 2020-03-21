52179d82676cde57586ff7a2aa76e88e8ded615d
LiamReilly <liamreilly22@yahoo.com>
2020-03-20 21:45:20 -0400

    Added camera movement, start of blend trees, and agent navigation by click

---
+++ Assets/Scripts/RaycastHitBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastHitBall : MonoBehaviour
+{
+    private float timer;
+    public const float STAY_TIME = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = STAY_TIME;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0){
+            Destroy(this.gameObject);
+        }
+    }
+}
