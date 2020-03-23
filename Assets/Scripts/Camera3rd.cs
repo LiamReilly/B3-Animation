48c5ff82d9f8a0ada9a21e3c5a9e263a73053beb
aroy99 <aaron.roy99@yahoo.com>
2020-03-23 16:34:41 -0400

    Made it so camera now rotates when player does

--- Assets/Scripts/Camera3rd.cs
+++ Assets/Scripts/Camera3rd.cs
@@ -6,13 +6,18 @@ using UnityEngine;
 public class Camera3rd : MonoBehaviour
 {
     public Transform Father;
+    public float yOffset = 1.5f;
+    public float xDownRot = 0;
     Quaternion originalRot;
+    Vector3 offset;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         Father = GameObject.FindGameObjectWithTag("Controllable").GetComponent<Transform>();
         originalRot = Father.rotation; 
+        offset = new Vector3(0, yOffset, 0);
     }
 
     // Update is called once per frame
@@ -20,8 +25,19 @@ public class Camera3rd : MonoBehaviour
     {
         
         Quaternion target = Father.rotation;
-        transform.position = Father.position + new Vector3(0.4f, 2.45f, -2.35f);
-        transform.rotation = originalRot;
+        transform.position = Father.position - Father.transform.forward*2 + new Vector3(0, yOffset, 0);
+        transform.rotation = Father.rotation;
+        //Point down at player
+        transform.Rotate(xDownRot, 0, 0);
+    }
+
+    //Removes the Y component of a vector and normalizes it to prevent drift up and down when moving
+    Vector3 fixYAxis(Vector3 vector)
+    {   
+        Vector3 returnee = new Vector3(vector.x, 0, vector.z);
+        returnee.Normalize();
+        return returnee;
+
     }
    
     
