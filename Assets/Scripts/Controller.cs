caa2da2e8c782d2fbe42d6249e15952d268c2447
LiamReilly <liamreilly22@yahoo.com>
2020-03-30 20:49:39 -0400

    attempted to fix jump, minor other fixes

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -9,6 +9,7 @@ public class Controller : MonoBehaviour
     
     Vector2 smoothDeltaPosition = Vector2.zero;
     Vector2 velocity = Vector2.zero;
+    Vector3 GroundPoint;
     
     ObjectSelection Selectionmanager;
     Animator anim;
@@ -30,6 +31,77 @@ public class Controller : MonoBehaviour
         feet.setOT(() => jumping = false);
     }
 
+    //changed to add force instead of translate didn't help
+    /*private void FixedUpdate()
+    {
+        if (!cam.isActiveAndEnabled)
+        {
+            float vert = Input.GetAxis("Vertical") * Time.deltaTime * Speed;
+            float horz = Input.GetAxis("Horizontal") * Time.deltaTime * Speed;
+            //Debug.Log(horz);
+            //Debug.Log(vert);
+            velocity = rb.velocity;
+            anim.SetFloat("velocityx", horz * Speed);
+            anim.SetFloat("velocityy", vert * Speed);
+            if (horz != 0 || vert != 0)
+            {
+                move = true;
+            }
+            else
+            {
+                move = false;
+            }
+
+            if (horz < 0) anim.speed = 2;
+            else anim.speed = 1;
+            //Debug.Log(velocity.x);
+
+            anim.SetBool("Move", move);
+            if (Input.GetKey(KeyCode.Space) && !jumping)
+            {
+                anim.SetTrigger("Jump");
+                jumping = true;
+                StartCoroutine(wait3Seconds());
+            }
+            if (Input.GetKey(KeyCode.Delete))
+            {
+                anim.SetTrigger("Die");
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                shouldTurn = true;
+                anim.SetBool("shouldturn", shouldTurn);
+                anim.SetFloat("turn", -1f);
+            }
+            else if (Input.GetKey(KeyCode.E))
+            {
+                shouldTurn = true;
+                anim.SetBool("shouldturn", shouldTurn);
+                anim.SetFloat("turn", 1f);
+            }
+            else
+            {
+                shouldTurn = false;
+                anim.SetBool("shouldturn", shouldTurn);
+            }
+            if (!shouldTurn && !jumping)
+            {
+                rb.AddForce(horz, 0f, vert);
+            }
+            if (!jumping)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
+                Debug.DrawLine(transform.position, Vector2.down, Color.magenta);
+                if (hit.collider != null)
+                {
+                    Debug.Log("hit collider");
+                    //float distance = Mathf.Abs(hit.point.y - transform.position.y);
+                    //gameObject.transform.Translate(0f, distance, 0f);
+                    gameObject.transform.position = hit.point;
+                }
+            }
+        }
+    }*/
     // Update is called once per frame
     void Update()
     {
@@ -51,8 +123,8 @@ public class Controller : MonoBehaviour
                 move = false;
             }
 
-            /*if (horz < 0) anim.speed = 2;
-            else anim.speed = 1;*/
+            //if (horz < 0) anim.speed = 2;
+            //else anim.speed = 1;
             //Debug.Log(velocity.x);
             
             anim.SetBool("Move", move);
@@ -60,7 +132,7 @@ public class Controller : MonoBehaviour
             {
                 anim.SetTrigger("Jump");
                 jumping = true;
-                StartCoroutine(wait1Seconds());
+                StartCoroutine(wait3Seconds());
             }
             if (Input.GetKey(KeyCode.Delete))
             {
@@ -81,16 +153,31 @@ public class Controller : MonoBehaviour
                 shouldTurn = false;
                 anim.SetBool("shouldturn", shouldTurn);
             } 
-            //if (!shouldTurn || !jumping)
-            //{
+            if (!shouldTurn && !jumping)
+            {
                 gameObject.transform.Translate(horz, 0, vert);
-            //}
+            }
+            if (!jumping)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
+                Debug.DrawLine(transform.position, Vector2.down, Color.magenta);
+                if (hit.collider != null)
+                {
+                    Debug.Log("hit collider");
+                    //float distance = Mathf.Abs(hit.point.y - transform.position.y);
+                    //gameObject.transform.Translate(0f, distance, 0f);
+                    gameObject.transform.position = hit.point;
+                }
+            }
+            
         }
-        IEnumerator wait1Seconds()
-        {
-            yield return new WaitForSeconds(1);
-            jumping = false;
-        }
+        
+        
 
+    }
+    IEnumerator wait3Seconds()
+    {
+        yield return new WaitForSeconds(1.5f);
+        jumping = false;
     }
 }
